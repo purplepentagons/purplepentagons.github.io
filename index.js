941148950a2bdb1545c9240b218e8db1f5e22a8d
purplepentagons <197023143+purplepentagons@users.noreply.github.com>
2025-04-11 23:32:18 -0400

    redesign it again

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     textBackground = document.querySelectorAll(".text-background")
     textBackground.forEach(element => {
         text = document.createTextNode(
-            "PURPLEPENTAGONS".repeat(10)
+            "PURPLE_PENTAGONS_".repeat(10)
         );
         element.appendChild(text);
     });
